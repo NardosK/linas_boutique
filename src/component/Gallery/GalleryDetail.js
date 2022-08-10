8a6f2bad616a2e9ec60ce1368705c10a15a1bb95
Girma32 <girmagizachew2016@gmail.com>
2022-08-09 22:54:41 -0400

    home testimonial

--- src/component/Gallery/GalleryDetail.js
+++ src/component/Gallery/GalleryDetail.js
@@ -23,8 +23,9 @@
 import * as React from 'react';
 
 import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
+import { CCloseButton } from '@coreui/react'
+import CloseIcon from '@mui/icons-material/Close';
 
 
 
@@ -34,28 +35,35 @@ export default function BasicModal({ imagePath }) {
   const handleClose = () => setOpen(false);
 
   return (
-    <div className="h-72 md:h-[32rem] lg:h-[40rem] lg:w-1/3 w-full md:w-1/2 p-4">
-      <div className="flex h-full w-full relative" onClick={handleOpen}>
-        <img
-          alt="gallery"
-          className="w-full h-full rounded-lg object-fill"
-          src={imagePath}
-        /></div>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-        className="w-[80vh] h-[80vh] m-auto"
-      >
-        <div className="w-full h-full">
+    <>
+
+      <div
+
+        className="h-72 md:h-[32rem] lg:h-[40rem] lg:w-1/3 w-full md:w-1/2 p-4">
+        <div className="flex h-full w-full relative" onClick={handleOpen}>
           <img
             alt="gallery"
-            className="h-full w-full rounded-lg object-fill"
+            className="w-full h-full rounded-lg object-fill"
             src={imagePath}
-          />
-        </div>
-      </Modal>
-    </div>
+          /></div>
+
+
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+          className="w-[80vh] h-[80vh] m-auto"
+        >
+
+          <div className="w-full h-full">
+            <img
+              alt="gallery"
+              className="h-full w-full rounded-lg object-fill"
+              src={imagePath}
+            />
+          </div>
+        </Modal>
+      </div ></>
   );
 }
